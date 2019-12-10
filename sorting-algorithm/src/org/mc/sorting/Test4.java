2b6842556134c434df617351564cf08534530efe
machao <chao.ma06>
2019-12-10 11:43:06 +0800

    cc

--- sorting-algorithm/src/org/mc/sorting/Test4.java
+++ sorting-algorithm/src/org/mc/sorting/Test4.java
@@ -10,6 +10,7 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("bb");
+        System.out.println("cc::");
     }
 
 }
